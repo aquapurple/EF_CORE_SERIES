ccb0f853bcc6cbffdf6f1e46f7514e7acfe6beef
APAC\ANISJOH <allegis.john@mercedes-benz.com>
2024-03-07 16:22:02 +0530

    Adding Icollection property in Student class

---
+++ Check_Context_Diff_Proj/Entities/Evaluation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Context_Diff_Proj.Entities
+{
+    public class Evaluation
+    {
+        [Column("EvaluationId")]
+        public Guid Id { get; set; }
+        [Required]
+        public int Grade { get; set; }
+        public string AdditionalExplanation { get; set; }
+
+        //one-2Many Relationship btw Student and Evaluation
+        public ICollection<Evaluation> Evaluations { get; set; }
+    }
+}
