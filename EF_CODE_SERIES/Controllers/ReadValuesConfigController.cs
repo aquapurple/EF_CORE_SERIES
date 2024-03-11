d19ed5aeb710c4a6550bd90bef1cdb7691a09925
APAC\ANISJOH <allegis.john@mercedes-benz.com>
2024-03-11 16:55:51 +0530

    Getting value from appsettings

---
+++ EF_CODE_SERIES/Controllers/ReadValuesConfigController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_CODE_SERIES.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadValuesConfigController : ControllerBase
+    {
+private readonly IConfiguration _configuration;
+        public ReadValuesConfigController(IConfiguration configuration)
+        {
+            _configuration= configuration;
+          }
+
+
+
+        [HttpGet]
+        [Route("api/ReadValuesFromAppSett")]
+        public string ReadValuesFromAppSett()
+        {
+            var students = _configuration.GetValue<string>("ConnectionStrings:sqlConnection1");
+            var names = _configuration.GetValue<string>("Myvalues:Name");
+
+            return students.ToString() + names.ToString();
+        }
+    }
+}
